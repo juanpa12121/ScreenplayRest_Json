de2e93746e6018d353837389ca8aec56bd5d1103
2616479 <2616479@tcs.com>
2023-04-08 18:20:14 -0500

    Commit para revisar en personal por qué manda en null los objetos anidados del json

--- ScreenplayRest_Json/src/test/java/co/com/tcs/certification/json/stepdefinitions/GetUsersStepDefinitions.java
+++ ScreenplayRest_Json/src/test/java/co/com/tcs/certification/json/stepdefinitions/GetUsersStepDefinitions.java
@@ -1,8 +1,9 @@
 package co.com.tcs.certification.json.stepdefinitions;
-
+import co.com.tcs.certification.json.models.DataGetUser;
+import co.com.tcs.certification.json.models.DataGetUserBuilder;
+import co.com.tcs.certification.json.questions.ValidateDataExpected;
 import co.com.tcs.certification.json.questions.ValidateStatusCode;
 import co.com.tcs.certification.json.tasks.GetUsersJson;
-import co.com.tcs.certification.json.utils.Constants;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -12,6 +13,8 @@ import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import org.hamcrest.Matchers;
+
+import java.util.List;
 
 import static co.com.tcs.certification.json.utils.Constants.ACTOR_NAME;
 import static co.com.tcs.certification.json.utils.Constants.BASE_ENDPOINT_JSON;
@@ -35,10 +38,17 @@ public class GetUsersStepDefinitions {
         OnStage.theActorInTheSpotlight().attemptsTo(GetUsersJson.getUserList());
     }
 
+    //@Scenario1
     @Then("^The user validates the status code (\\d+)$")
     public void theUserValidatesTheStatusCode(int statusCode) {
         OnStage.theActorInTheSpotlight()
                 .should(GivenWhenThen.seeThat(ValidateStatusCode.getStatusCode(),
                         Matchers.equalTo(200)));
+    }
+
+    //@Scenario2
+    @Then("^The user validates that get response contains data expected$")
+    public void theUserValidatesThatGetResponseContainsDataExpected(List<DataGetUserBuilder> dataGetList) {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(ValidateDataExpected.validateDataExpected(dataGetList.get(0))));
     }
 }
--- ScreenplayRest_Json/src/test/resources/features/get_users.feature
+++ ScreenplayRest_Json/src/test/resources/features/get_users.feature
@@ -10,3 +10,14 @@ Feature: Get list of users on JSON Placeholder API
   Scenario: Request list of users correctly
     When The user makes the query request
     Then The user validates the status code 200
+
+  @Scenario2
+  Scenario Outline: Request list of users in the API and validate the response fields
+    When The user makes the query request
+    Then The user validates that get response contains data expected
+      | id   | name   | username   | email   | street   | suite   | city   | zipCode   | lat   | lng   | phone   | website   | companyName   | catchPhrase   | bs   |
+      | <id> | <name> | <username> | <email> | <street> | <suite> | <city> | <zipCode> | <lat> | <lng> | <phone> | <website> | <companyName> | <catchPhrase> | <bs> |
+    Examples:
+      | id | name         | username  | email             | street        | suite | city        | zipCode    | lat      | lng      | phone               | website       | companyName  | catchPhrase                    | bs                               |
+      | 2  | Ervin Howell | Antonette | Shanna@melissa.tv | Victor Plains | 879   | Wisokyburgh | 90566-7771 | -43.9509 | -34.4618 | 010-692-6593 x09125 | anastasia.net | Deckow-Crist | Proactive didactic contingency | synergize scalable supply-chains |
+
